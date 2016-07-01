abc50673739666b82313338b5fe92e5e0d9bc67a
Steve Lacy <me@slacy.me>
2016-07-01 15:56:30 -0700

    start creating mocks

---
+++ test/mocks/orders.js
@@ -0,0 +1,5 @@
+export const order = {
+  products: [],
+  id: '1234455',
+  status: 'new_order'
+}
---
+++ test/mocks/users.js
@@ -0,0 +1,19 @@
+export const admin = {
+  email: 'admin@email.co',
+  id: '124',
+  local: {
+    accessToken: '5cba76d1767d602f34ac7ffc0bf9d26a987e3c564b253424dc10640ed027a9ca',
+    id: 'admin@email.co'
+  },
+  role: 'admin',
+  password: 'testpass'
+}
+export const user = {
+  email: 'valid@email.co',
+  id: '123',
+  local: {
+    accessToken: '5cba76d1767d602f34ac7ffc0bf9d26a987e3c564b253424dc10640ed027a9ca',
+    id: 'valid@email.co'
+  },
+  password: 'testpass'
+}
