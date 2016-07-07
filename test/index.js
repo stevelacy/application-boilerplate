25806130107a221ee46c3de206de45193028c46c
Steve Lacy <me@slacy.me>
2016-07-07 10:31:06 -0700

    delete all data before running tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,21 @@
+import async from 'async'
+import config from 'app-config-chain'
 import thinky from '../api/connections/rethink'
 import redis from '../api/connections/redis'
 import api from '../api'
 
+
+before((done) => {
+  // ensure the env and db is testing
+  const tables = [
+    'User',
+    'Order',
+    'Product'
+  ]
+  async.each(tables, (table, cb) => {
+    thinky.r.table(table).delete().run(cb)
+  }, done)
+})
 after(() => {
   thinky.r.getPool().drain()
   if (redis.close) redis.close()
