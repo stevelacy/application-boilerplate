a9a56e9342f4e8469222e9c2fc0565525fadf8ea
Steve Lacy <me@slacy.me>
2016-07-18 11:16:57 -0700

    splitout new project creator + add infinite scrolling

---
+++ client/views/Admin/Products/NewProduct.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import { PropTypes, Component } from 'shasta'
+// import classes from 'classnames'
+import actions from 'core/actions'
+import './index.sass'
+
+export default class AdminNewProduct extends Component {
+  static displayName = 'AdminProductsComponent'
+  static propTypes = {
+    productcategories: PropTypes.list
+  }
+
+  static defaultState = {
+    name: '',
+    categoryId: '',
+    description: '',
+    gender: '',
+    sku: '',
+    image: '',
+    enabled: true
+  }
+
+  handleInput (e) {
+    let obj = {}
+    obj[e.target.name] = e.target.value
+    this.setState(obj)
+  }
+
+  handleSave () {
+    actions.api.productsources.create({
+      subset: 'product',
+      body: this.state
+    })
+  }
+
+  render () {
+    return (
+      <div className='new-product'>
+        <input
+          name='name'
+          className='input'
+          value={this.state.name}
+          placeholder='product name'
+          onChange={this.handleInput} />
+        <input
+          name='description'
+          className='input'
+          value={this.state.description}
+          placeholder='product description'
+          onChange={this.handleInput} />
+        <input
+          name='sku'
+          className='input'
+          value={this.state.sku}
+          placeholder='product sku'
+          onChange={this.handleInput} />
+        <input
+          name='image'
+          className='input'
+          value={this.state.image}
+          placeholder='product image url'
+          onChange={this.handleInput} />
+
+        <select
+          className='input'
+          name='categoryId'
+          onChange={this.handleInput}
+          value={this.state.categoryId}>
+          <option value='' disabled> choose category </option>
+          {
+            this.props.productcategories.map((item) => {
+              return (
+                <option
+                  key={item.get('id')}
+                  value={item.get('id')}>
+                  {item.get('name') + '/' + item.get('category')}
+                </option>
+              )
+            })
+          }
+          </select>
+
+        <select
+          className='input'
+          name='gender'
+          onChange={this.handleInput}
+          value={this.state.gender}>
+          <option value='' disabled> choose gender </option>
+          <option value='female'> female </option>
+          <option value='male'> male </option>
+          <option value='male/female'> both </option>
+        </select>
+        <button
+          className='button green'
+          onClick={this.handleSave}>
+          SAVE
+          </button>
+      </div>
+    )
+  }
+}
--- client/views/Admin/Products/index.js
+++ client/views/Admin/Products/index.js
@@ -4,6 +4,8 @@ import { PropTypes, connect } from 'shasta'
 import DataComponent from 'shasta-data-view'
 import actions from 'core/actions'
 import Title from 'components/Title'
+import InfiniteScroll from 'react-infinite'
+import NewProduct from './NewProduct'
 import './index.sass'
 
 @connect({
@@ -20,18 +22,12 @@ export default class AdminProductsComponent extends DataComponent {
   }
 
   static defaultState = {
-    name: '',
-    categoryId: '',
-    description: '',
-    gender: '',
-    sku: '',
-    image: '',
-    enabled: true
+    containerHeight: window.innerHeight
   }
 
-  // componentWillUnmount () {
-  //   clearInterval(this.refetch)
-  // }
+  static defaultProps = {
+    elementHeight: 100
+  }
 
   resolveData () {
     actions.api.productcategories.find({
@@ -42,103 +38,34 @@ export default class AdminProductsComponent extends DataComponent {
     })
   }
 
-  handleInput (e) {
-    let obj = {}
-    obj[e.target.name] = e.target.value
-    this.setState(obj)
-  }
-
-  handleSave () {
-    actions.api.productsources.create({
-      subset: 'product',
-      body: this.state
-    })
-  }
-
-  renderNewProduct () {
-    return (
-      <div className='new-product'>
-        <input
-          name='name'
-          className='input'
-          value={this.state.name}
-          placeholder='product name'
-          onChange={this.handleInput} />
-        <input
-          name='description'
-          className='input'
-          value={this.state.description}
-          placeholder='product description'
-          onChange={this.handleInput} />
-        <input
-          name='sku'
-          className='input'
-          value={this.state.sku}
-          placeholder='product sku'
-          onChange={this.handleInput} />
-        <input
-          name='image'
-          className='input'
-          value={this.state.image}
-          placeholder='product image url'
-          onChange={this.handleInput} />
-
-        <select
-          className='input'
-          name='categoryId'
-          onChange={this.handleInput}
-          value={this.state.categoryId}>
-          <option value='' disabled> choose category </option>
-          {
-            this.props.productcategories.map((item) => {
-              return (
-                <option
-                  key={item.get('id')}
-                  value={item.get('id')}>
-                  {item.get('name') + '/' + item.get('category')}
-                </option>
-              )
-            })
-          }
-          </select>
-
-        <select
-          className='input'
-          name='gender'
-          onChange={this.handleInput}
-          value={this.state.gender}>
-          <option value='' disabled> choose gender </option>
-          <option value='female'> female </option>
-          <option value='male'> male </option>
-          <option value='male/female'> both </option>
-        </select>
-        <button
-          className='button green'
-          onClick={this.handleSave}>
-          SAVE
-          </button>
-      </div>
-    )
-  }
-
   render() {
     if (!this.props.products) return null
     return (
       <div className='admin-products-component block item-list'>
       <Title> Products </Title>
       <div className='hr' />
-      {this.renderNewProduct()}
-      {
-        this.props.products.map((order) => {
-          return (
-            <div
-              key={order.get('id')}
-              className='item'>
-              {order.get('id')}
-            </div>
-          )
-        })
-      }
+      <NewProduct productcategories={this.props.productcategories} />
+
+      <InfiniteScroll
+        className='list-component'
+        containerHeight={this.state.containerHeight}
+        infiniteLoadBeginEdgeOffset={250}
+        elementHeight={this.props.elementHeight}>
+        {
+          this.props.products.map((order) => {
+            return (
+              <div
+                style={{ height: this.props.elementHeight }}
+                key={order.get('id')}
+                className='list-item'>
+                <img className='image' src={order.get('image')} />
+                {order.get('id')}
+              </div>
+            )
+          })
+        }
+
+      </InfiniteScroll>
       </div>
     )
   }
